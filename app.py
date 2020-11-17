7b4dd88c0b2608510b23cd4b81bd879268079479
Jeongum <aldlswjddma@naver.com>
2020-11-18 02:19:38 +0900

    merge stt+meeting+chat to meeting

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from flask_login import LoginManager
 from dotenv import load_dotenv
 
 from route.login_route import login_route
-from route.chat_route import chat_route
-from route.stt_route import stt_route
 from route.meeting_route import meeting_route
 from route.minute_route import minute_route
 
@@ -26,8 +24,6 @@ db = SQLAlchemy()
 db.init_app(app)
 
 app.register_blueprint(login_route)
-app.register_blueprint(chat_route)
-app.register_blueprint(stt_route)
 app.register_blueprint(meeting_route)
 app.register_blueprint(minute_route)
 
