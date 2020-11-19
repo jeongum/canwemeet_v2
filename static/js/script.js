d0d46080130bbc01017d2f6427fd5b216e71e7c9
HyeIn-Kim <fallingstars2749@gmail.com>
2020-11-19 09:04:55 +0900

    Add minute-details tab

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   
+    $('#minute-container .tab-menu').click(function(){
+      var tab_id = $(this).attr('data-tab');
+   
+      $('#minute-container .tab-menu').removeClass('current');
+      $('.tab-content').removeClass('current');
+   
+      $(this).addClass('current');
+      $("#"+tab_id).addClass('current');
+    })
+  })
